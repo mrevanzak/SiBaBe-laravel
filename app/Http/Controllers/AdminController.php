73b72d78d70510294ea04c201e1d967b14f5a2e5
mrevanzak <mrevanzak@gmail.com>
2023-02-25 13:15:30 +0700

    refactor: use seeder

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,50 +9,39 @@ use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
-    public function __construct()
+    public function login(Request $request): JsonResponse
     {
-        Admin::firstOrCreate(['username' => 'admin'], [
-            'username' => 'admin',
-            'name' => 'admin',
-            'password' => bcrypt('admin'),
-            'email' => 'admin@sibabe.app',
-            'phone' => '081234567890',
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'password' => 'required|string',
         ]);
+
+        if ($validator->fails()) {
+            return $this->error(
+                'failed to login',
+                'username or password is invalid',
+                422
+            );
+        }
+
+        if (! auth('admins')->attempt($validator->validated())) {
+            return $this->error(
+                'failed to login',
+                'username or password is invalid',
+                401
+            );
+        }
+
+        $admin = Admin::where('username', $request->username)->first();
+        $token = $admin->createToken('auth_token')->plainTextToken;
+
+        return $this->success(
+            'login success',
+            [
+                'username' => $admin->username,
+                'name' => $admin->name,
+                'token' => $token,
+            ]
+        );
     }
-
-      public function login(Request $request): JsonResponse
-      {
-          $validator = Validator::make($request->all(), [
-              'username' => 'required|string',
-              'password' => 'required|string',
-          ]);
-
-          if ($validator->fails()) {
-              return $this->error(
-                  'failed to login',
-                  'username or password is invalid',
-                  422
-              );
-          }
-
-          if (! auth('admins')->attempt($validator->validated())) {
-              return $this->error(
-                  'failed to login',
-                  'username or password is invalid',
-                  401
-              );
-          }
-
-          $admin = Admin::where('username', $request->username)->first();
-          $token = $admin->createToken('auth_token')->plainTextToken;
-
-          return $this->success(
-              'login success',
-              [
-                  'username' => $admin->username,
-                  'name' => $admin->name,
-                  'token' => $token,
-              ]
-          );
-      }
 }
