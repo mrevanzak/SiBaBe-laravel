a8f6e9b03a3f0c860de7295eada87d4817e1886c
mrevanzak <mrevanzak@gmail.com>
2023-02-25 16:22:33 +0700

    feat(ProductController): improvement and fix for admin CRUD method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Feedback;
+use App\Models\Feedback_Pemesanan;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -14,9 +16,18 @@ class ProductController extends Controller
      */
     public function index(): JsonResponse
     {
+        $products = Product::all();
+        foreach ($products as $product) {
+            $product->reviews = Feedback::all()->where('id_produk', $product->id);
+
+            foreach ($product->reviews as $review) {
+                $review->user = Feedback_Pemesanan::all()->where('id_feedback', $review->id)->first()->username;
+            }
+        }
+
         return $this->success(
             'Succes get all products',
-            Product::all(),
+            $products,
         );
     }
 
@@ -25,53 +36,104 @@ class ProductController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        return Product::create($request->all());
-    }
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:products,name',
+            'price' => 'required|integer',
+            'description' => 'required|string',
+            'stock' => 'required|integer',
+            'image' => 'required|string',
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(int $id): JsonResponse
-    {
-        $product = Product::where('id', $id)->first();
-
-        if (! $product) {
+        if ($validator->fails()) {
             return $this->error(
-                'Failed to get product by id',
-                'Product not found',
-                404
+                'Failed to create product',
+                $validator->errors()->first(),
+                400
             );
         }
 
-        $feedback = Feedback::where('id_produk', $id)->get();
+        $product = Product::create($validator->validated());
 
-        if (! $feedback) {
+        if (! $product) {
             return $this->error(
-                '',
-                'Feedback not found',
-                404
+                'Failed to create product',
+                'Failed to create product',
+                500
             );
         }
 
         return $this->success(
-            $product,
-            'Success get product'
+            'Success create product',
+            $product
         );
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): JsonResponse
+    public function update(Request $request, int $id): JsonResponse
     {
-        return Product::findOrFail($id)->update($request->all());
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'price' => 'required|integer',
+            'description' => 'required|string',
+            'stock' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error(
+                'Failed to update product',
+                $validator->errors()->first(),
+                400
+            );
+        }
+
+        $product = Product::findOrFail($id);
+
+        if (! $product->update($validator->validated())) {
+            return $this->error(
+                'Failed to update product',
+                'Failed to update product',
+                500
+            );
+        }
+
+        return $this->success(
+            'Success update product',
+            $product
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): JsonResponse
+    public function destroy(int $id): JsonResponse
     {
-        return Product::findOrFail($id)->delete();
+        $validator = Validator::make(['id' => $id], [
+            'id' => 'required|integer|exists:products,id',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error(
+                'Failed to delete product',
+                $validator->errors()->first(),
+                400
+            );
+        }
+
+        $product = Product::findOrFail($id);
+
+        if (! $product->delete()) {
+            return $this->error(
+                'Failed to delete product',
+                'Failed to delete product',
+                500
+            );
+        }
+
+        return $this->success(
+            'Success delete product',
+            null
+        );
     }
 }
